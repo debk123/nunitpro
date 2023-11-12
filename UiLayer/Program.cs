1e614ae2a1c36efb28a2412660ab4b7871ff4a99
raj <raj@gmail.com>
2023-11-13 02:26:14 +0530

    first

---
+++ UiLayer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UiLayer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to jen");
+        }
+
+        public static string pass()
+        {
+            return "pass";
+        }
+    }
+}
---
+++ unitpro/TestProj.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UiLayer;
+namespace unitpro
+{
+
+   [TestFixture]
+    public class TestProj
+    {
+        [Test]
+        public void Test1()
+        {
+            var r = Program.pass();
+
+            Assert.That("pass", Is.EqualTo("pass"));
+
+        }
+    }
+}
